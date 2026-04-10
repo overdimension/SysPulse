367cb62aa165842e94d19d287899295eac0967a3
Alex <officialalexdemchenko@gmail.com>
2026-04-10 18:24:56 +0300

    implement abstract BaseCollector class

---
+++ collectors/base.py
@@ -0,0 +1,32 @@
+from abc import ABC, abstractmethod
+import datetime
+
+class BaseCollector(ABC):
+    def __init__(self):
+        self.name = self.__class__.__name__.replace('Collector', '').lower()
+
+    @abstractmethod
+    def collect(self) -> dict:
+        """
+        Реализация сбора данных в дочерних классах
+        """
+        pass
+
+    def get_data(self) -> dict:
+        """
+        Метод для получения данных с добавлением временной метки
+        """
+        try:
+            return {
+                "timestamp": datetime.datetime.now().isoformat(),
+                "collector": self.name,
+                "status": "success",
+                "metrics": self.collect()
+            }
+        except Exception as e:
+            return {
+                "timestamp": datetime.datetime.now().isoformat(),
+                "collector": self.name,
+                "status": "error",
+                "error_message": str(e)
+            }
