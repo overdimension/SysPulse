64d949977fbd9cd115e1b5710bbf5521b070106a
Alex <officialalexdemchenko@gmail.com>
2026-04-10 18:49:11 +0300

    implement MonitoringAgent core logic

---
+++ core/agent.py
@@ -0,0 +1,43 @@
+import time
+from collectors.cpu import CPUCollector
+from collectors.memory import MemoryCollector
+
+class MonitoringAgent:
+    def __init__(self, interval=5):
+        self.interval = interval
+        self.collectors = [
+            CPUCollector(),
+            MemoryCollector()
+        ]
+        self.running = False
+
+    """Запуск бесконечного цикла мониторинга"""
+    def start(self):
+        self.running = True
+        print("Monitoring Agent started.")
+        print("Ctrl+C to stop.")
+
+        try:
+            while self.running:
+                self.proccess_tick()
+                time.sleep(self.interval)
+        except KeyboardInterrupt:
+            self.stop()
+
+    """Цикл опроса датчиков"""
+    def proccess_tick(self):
+        print(f"\n--- Collecting data at {time.strftime('%Y-%m-%d %H:%M:%S')} ---") 
+        for collector in self.collectors:
+            data = collector.get_data()
+        
+            if data["status"] == "success":
+                metrics = data['metrics']
+                metrics_str = ", ".join([f"{key}: {value}" for key, value in metrics.items()])
+                print(f"[{data['collector'].upper()}]: {metrics_str}")
+            else:
+                print(f"Mistake in {data['collector']}: {data.get('message', 'Unknown error')}")
+
+    """Остановка агента"""
+    def stop(self):
+        self.running = False
+        print("\nMonitoring Agent stopped.")
