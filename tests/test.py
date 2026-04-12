62219300a93e91f8e041e0022d87618b48316731
Alex <officialalexdemchenko@gmail.com>
2026-04-12 22:14:25 +0300

    update and add disk and processes collectors' testing

--- tests/test.py
+++ tests/test.py
@@ -1,13 +1,38 @@
 from collectors.cpu import CPUCollector
 from collectors.memory import MemoryCollector
+from collectors.disk import DiskCollector
+from collectors.processes import ProcessesCollector
 
-monitors = [CPUCollector(), MemoryCollector()]
+from tabulate import tabulate
 
-print("--SysPulse System Monitor--")
+def run_test():
+    monitors = [
+        CPUCollector(),
+        MemoryCollector(),
+        DiskCollector(),
+        ProcessesCollector()
+    ]
 
-for m in monitors:
-    result = m.get_data()
-    if result["status"] == "success":
-        print(f"[{result['collector'].upper()}] Usage: {result['metrics']}")
-    else:
-        print(f"Error in {result['collector']}: {result['message']}")
+    print("Running tests for collectors...\n")
+
+    for m in monitors:
+        result = m.get_data()
+        name = result['collector'].upper()
+        timestamp = result['timestamp']
+
+        if result['status'] == 'success':
+            print(f"[{timestamp}] {name} Collector: SUCCESS")
+
+            for key, value in result['metrics'].items():
+                print(f"  -> {key}: {value}")
+        else:
+            print(f"[{timestamp}] {name} Collector: ERROR - {result.get('message', 'Unknown error')}")
+
+        
+        if result["collector"] == "processes":
+            top_processes = result['metrics']['top_processes']
+
+            print(tabulate(top_processes, headers="keys", tablefmt="grid"))
+
+if __name__ == "__main__":
+    run_test()
