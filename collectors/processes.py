a20b021c1af52cf667aa123367c1a373b9884ca9
Alex <officialalexdemchenko@gmail.com>
2026-04-15 17:51:45 +0300

    implement stream generator for memory-efficient process tracking

--- collectors/processes.py
+++ collectors/processes.py
@@ -16,3 +16,11 @@ class ProcessesCollector(BaseCollector):
         return {
             "top_processes": top_processes
         }
+
+    def stream_processes(self):
+        """Генератор для потоковой передачи данных о процессах"""
+        for proc in psutil.process_iter(['pid', 'name', 'username', 'cpu_percent', 'memory_percent']):
+            try:
+                yield proc.info
+            except (psutil.NoSuchProcess, psutil.AccessDenied):
+                continue
