b5fd196a46e4fbff550946bb04e7438dd0181f56
Alex <officialalexdemchenko@gmail.com>
2026-04-16 22:23:05 +0300

    integrate log analysis into CLI via --analyze flag

--- cli/main.py
+++ cli/main.py
@@ -1,6 +1,11 @@
 from core.agent import MonitoringAgent
+from utils.reader import analyze_logs
+import sys
 
 def run():
+    if len(sys.argv) > 1 and sys.argv[1] == "--analyze":
+        analyze_logs("logs/monitoring_agent.log")
+
     agent = MonitoringAgent(interval=2)
     agent.start()
 
