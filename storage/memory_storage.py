daf523bfab66b611695bcc55f9878b9f39ccb6a1
Alex <officialalexdemchenko@gmail.com>
2026-04-18 21:39:48 +0300

    implement volatile MemoryStorage for fast metrics caching

---
+++ storage/memory_storage.py
@@ -0,0 +1,20 @@
+from storage.base import BaseStorage
+import logging
+
+class MemoryStorage(BaseStorage):
+    def __init__(self):
+        self._data = []
+        logging.info("The memory storage has been initialized.")
+
+    def save(self, collector_name: str, metrics: dict):
+        entry = {
+            "collector": collector_name,
+            "metrics": metrics
+        }
+        self._data.append(entry)
+
+        if len(self._data) > 1000:
+            self._data.pop(0) 
+
+    def get_all(self):
+        return self._data
