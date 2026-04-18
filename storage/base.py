14b503344910acaa6a19b821d4766531a0d8fee3
Alex <officialalexdemchenko@gmail.com>
2026-04-18 21:39:40 +0300

    define abstract BaseStorage interface

---
+++ storage/base.py
@@ -0,0 +1,12 @@
+from abc import ABC, abstractmethod
+
+class BaseStorage(ABC):
+    @abstractmethod
+    def save(self, collector_name: str, metrics: dict):
+        """Метод для сохранения данных"""
+        pass
+
+    @abstractmethod
+    def get_all(self):
+        """Метод для получения всех накопленных данных"""
+        pass
